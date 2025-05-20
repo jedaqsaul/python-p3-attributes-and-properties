485cb0bd1baef22bf19183afc38d0aab39acffdc
jedaqsaul <jedaqsaul@gmail.com>
2025-05-20 21:21:13 +0300

    Completes the attributes and properties lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,45 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Fido", breed="Beagle"):
+        self.name = name
+        self.breed = breed
+
+    # -------- Name Property --------
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    # -------- Breed Property --------
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+
+
+
+dog1 = Dog(name="Rex", breed="Corgi")
+print(dog1.name)   # Rex
+print(dog1.breed)  # Corgi
+
+dog2 = Dog(name="", breed="Dinosaur")
+# => Name must be string between 1 and 25 characters.
+# => Breed must be in list of approved breeds.
+
+dog2.name = "Fluff"
+dog2.breed = "Pug"
+print(dog2.name)   # Fluff
+print(dog2.breed)  # Pug
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,44 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="John Doe", job="Sales"):
+        self.name = name
+        self.job = job
+
+    # ----- name property -----
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    # ----- job property -----
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+
+
+p1 = Person(name="alice walker", job="Finance")
+print(p1.name)  # Alice Walker
+print(p1.job)   # Finance
+
+p2 = Person(name="", job="Spy")
+# => Name must be string between 1 and 25 characters.
+# => Job must be in list of approved jobs.
+
+p2.name = "jane smith"
+p2.job = "ITC"
+print(p2.name)  # Jane Smith
+print(p2.job)   # ITC
